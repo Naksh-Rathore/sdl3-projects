e92ca4051c53c6fa7d0479382149afd47c8079f7
Naksh Rathore <naksh.rathore786@gmail.com>
2025-10-18 21:56:36 -0500

    Improve output messages and formatting in cords_mover

--- cords_mover/cords_mover.cpp
+++ cords_mover/cords_mover.cpp
@@ -19,18 +19,22 @@ int main(int argc, char* argv[]) {
 	bool isRunning = true;
 	std::array<int, 2> cords = { 0, 0 };
 
+	std::cout << "Starting cords: " << "\n";
+	std::cout << "X: " << cords[0] << "\n";
+	std::cout << "Y: " << cords[1] << "\n\n";
+
 	while (isRunning) {
 		int key = getKeyPress(isRunning);
 
 		/* w = 119
 		   a = 97
-	       s = 115
-	       d = 100 */
+		   s = 115
+		   d = 100 */
 
 		if (key == 119 || key == 97 || key == 115 || key == 100) {
 			cords = updateCords(key, cords);
 
-			std::cout << "New cords: " << "\n";
+			std::cout << "Updated Cords: " << "\n";
 			std::cout << "X: " << cords[0] << "\n";
 			std::cout << "Y: " << cords[1] << "\n\n";
 		}
@@ -39,18 +43,18 @@ int main(int argc, char* argv[]) {
 	SDL_Quit();
 }
 
-int getKeyPress(bool &isRunning) {
+int getKeyPress(bool& isRunning) {
 	SDL_Event event;
-	
+
 	while (SDL_PollEvent(&event)) {
 		if (event.type == SDL_EVENT_QUIT) {
 			isRunning = false;
 			return 0;
 		}
 
-		else if (event.type == SDL_EVENT_KEY_DOWN) 
+		else if (event.type == SDL_EVENT_KEY_DOWN)
 			return event.key.key;
-		
+
 		else
 			return 0;
 	}
@@ -70,14 +74,14 @@ SDL_Window* getWindow() {
 std::array<int, 2> updateCords(int key, std::array<int, 2> cords) {
 	/* w = 119
 	   a = 97
-       s = 115
-       d = 100 */
+	   s = 115
+	   d = 100 */
 
 	switch (key) {
-		case 119: cords[1] += 10; break;
-		case 115: cords[1] -= 10; break;
-		case 100: cords[0] += 10; break;
-		case 97: cords[0] -= 10; break;
+	case 119: cords[1] += 10; break;
+	case 115: cords[1] -= 10; break;
+	case 100: cords[0] += 10; break;
+	case 97: cords[0] -= 10; break;
 	}
 
 	return cords;
